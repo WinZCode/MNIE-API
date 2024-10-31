72e4704173da059d09b82a296ba77aba45930fc6
nqthang21 <nqthang21@gmail.com>
2024-10-31 09:53:12 +0700

    fix lint và thêm alias

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
         { id: 'id-2', name: 'Two' },
         { id: 'id-3', name: 'Three' },
         { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' },
+        { id: 'id-5', name: 'Five' }
       ],
       ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
       'id'
